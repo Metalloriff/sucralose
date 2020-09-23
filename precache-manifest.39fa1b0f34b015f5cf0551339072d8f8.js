5677ff38c9f8745aae4443666b1e35db79169dd3
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2020-09-23 16:31:36 -0400

    Mobile Testing
    need to make a public build to test on mobile

---
+++ precache-manifest.39fa1b0f34b015f5cf0551339072d8f8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76a00a603f6d87a102173365b095747",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6c01966dfada0935274",
+    "url": "./static/css/main.3dcf55f3.chunk.css"
+  },
+  {
+    "revision": "cef6536919f46ccdaa22",
+    "url": "./static/js/2.0ab55773.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "./static/js/2.0ab55773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c01966dfada0935274",
+    "url": "./static/js/main.541b8a11.chunk.js"
+  },
+  {
+    "revision": "09df1b88d09dcedb1a7f",
+    "url": "./static/js/runtime-main.5814a1e9.js"
+  },
+  {
+    "revision": "1fce70541cd225507e29c233e23f2cb2",
+    "url": "./static/media/Chevron.1fce7054.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "./static/media/Search.c6a0effb.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "./static/media/Sidebar.b3f49afd.svg"
+  },
+  {
+    "revision": "11e425a3c98dd13506e66e22097e2714",
+    "url": "./static/media/Spinner.11e425a3.svg"
+  }
+]);
